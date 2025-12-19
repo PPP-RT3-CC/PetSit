ccaedfcfa13d38eebb25e34148b0cca8428be7f4
yasmine <yasmine.elhakem@insat.ucar.tn>
2025-12-19 22:40:18 +0100

    add login and registration forms

--- Frontend/petsit-front/src/app/app.routes.ts
+++ Frontend/petsit-front/src/app/app.routes.ts
@@ -1,6 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home-component/home-component';
+import { Login } from './components/auth/login/login';
+import { Register } from './components/auth/register/register';
+import { MainLayout } from './components/layouts/main-layout/main-layout';
+import { AuthLayout } from './components/layouts/auth-layout/auth-layout';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    {   path: '', 
+        component: MainLayout,
+        children: [
+        {path:'', component: HomeComponent}
+        ]
+    },
+    {
+        path: '',
+        component: AuthLayout,
+        children: [
+        { path: 'login', component: Login },
+        { path: 'register', component: Register }
+    ]
+  }
 ];
