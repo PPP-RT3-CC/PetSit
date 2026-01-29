5906dc44da8c4a68cea73f34fdd82e8a2ca89ebb
yasmine <yasmine.elhakem@insat.ucar.tn>
2026-01-29 13:03:41 +0100

    add passport

--- Backend/petsit-back/src/auth/auth.module.ts
+++ Backend/petsit-back/src/auth/auth.module.ts
@@ -4,20 +4,25 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     //pour utiliser le repository User dans AuthService
     TypeOrmModule.forFeature([User]),
+
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
     //configuration du module JWT
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'my_secret_key',
+      secret: process.env.JWT_SECRET || 'supersecret',
       signOptions: {
         expiresIn: '3600', 
       },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- Backend/petsit-back/src/auth/jwt.strategy.ts
+++ Backend/petsit-back/src/auth/jwt.strategy.ts
@@ -1,18 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm/dist/common/typeorm.decorators';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
 
-  constructor() {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'supersecret',
+      secretOrKey: process.env.JWT_SECRET || 'supersecret',
     });
   }
 
   async validate(payload: any) {
-    return payload;
+    const user = await this.userRepository.findOne({
+      where: { id: payload.sub },
+      select: ['id', 'email', 'role'], 
+    });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;   
   }
 }
